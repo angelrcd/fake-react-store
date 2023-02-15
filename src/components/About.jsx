0f24e5d55791dc49b89476e9270875bde48d9b75
Angel Romero <mrangelrcd@gmail.com>
2023-02-15 14:40:25 +0100

    Added some info in about tab

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function About() {
   return (
-    <div>Lo primero buenas tardes</div>
+    <div className='m-6'>
+      <h1 className='text-3xl'>Angel Romero</h1>
+      <p className='text-xl'>Junior FullStack Developer</p>
+      <p><a target="_blank" href="https://www.linkedin.com/in/%C3%A1ngel-romero-cesteros-557700128/">LinkedIn</a></p>
+      <a target="_blank" href="https://github.com/angelrcd">GitHub</a>
+    </div>
   )
 }
 
